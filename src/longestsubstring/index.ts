a4848817abcdf401e6f46c50e00de62fe4f9c117
David Rearte <dengue8830@gmail.com>
2022-07-31 12:11:40 -0300

    optimized lengthOfLongestSubstring

--- src/longestsubstring/index.ts
+++ src/longestsubstring/index.ts
@@ -1,15 +1,15 @@
-function maxLength(tries: string[][]) {
+function maxLength(tries: string[]) {
   return tries.map((item) => item.length).sort((a, b) => a + b)[0];
 }
 
 export function lengthOfLongestSubstring(s: string): number {
-  const tries: string[][] = [[]];
+  const tries: string[] = [""];
   for (const char of s) {
     const last = tries.length - 1;
     if (tries[last].includes(char)) {
-      tries.push([]);
+      tries.push("");
     } else {
-      tries[last].push(char);
+      tries[last] += char;
     }
   }
 
