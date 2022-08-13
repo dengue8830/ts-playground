40f820a741e9903b2867c78231c08c20ac5397f6
David Rearte <dengue8830@gmail.com>
2022-08-13 15:54:56 -0300

    added cluster example

---
+++ src/cluster/index.mjs
@@ -0,0 +1,35 @@
+import cluster from "node:cluster";
+import { cpus } from "node:os";
+
+if (cluster.isPrimary) {
+  // ❌ Doesn't work. Executes the cb but the worker is not ready to listen yet.
+  // cluster.on("fork", (w) => {
+  //   w.send({ data: "hola" });
+  // });
+  for (let i = 0; i < cpus().length; i++) {
+    const w = cluster.fork();
+    w.on("message", (msg) => {
+      console.log("parent: worker says ", msg);
+    });
+    // ❌ Doesn't work.
+    // w.on("online", () => {
+    // w.send({ data: "hola" });
+    // });
+    // 🙅‍♂️ This works is error prone since it relies on the execution order of event loop.
+    // setTimeout(() => {
+    //   w.send({ data: "hola" });
+    // }, 100);
+  }
+  // ✅ This works.
+  cluster.on("online", () => {
+    Object.values(cluster.workers).forEach((w) => {
+      w.send({ data: "hi there" });
+    });
+  });
+} else {
+  process.on("message", (msg) => {
+    // Do initial work with msg data.
+    console.log("worker: parent says ", msg);
+    process.send({ data: "copied" });
+  });
+}
